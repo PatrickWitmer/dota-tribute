ab2e72daedd8028d58ccfb4887c7f7a414440371
witmerdesigns <patrick@witmerdesigns.com>
2017-03-03 22:00:14 -0700

    cleaning up app.js problems

--- app/app.js
+++ app/app.js
@@ -17,16 +17,6 @@ loadInitializers(App, config.modulePrefix);
 
 export default App;
 
-<<<<<<< HEAD
-//Check status code of api and return 200,300,400,500
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.opendota.com/api/heroStats", false);
-xhr.send();
-
-console.log(xhr.status);
-console.log(xhr.hero_id);
-//END STATUS CODE
-=======
 
 
   // $get.JSON("https://api.opendota.com/api/heroStats", function (data) {
@@ -38,4 +28,3 @@ console.log(xhr.hero_id);
 // xhr.send();
 
 // console.log(xhr.status);
->>>>>>> 52ee4ff9a77075af17a2435f2ce80617dcc4e669
